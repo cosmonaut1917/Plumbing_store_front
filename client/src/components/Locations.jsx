77dc0336232c86cdb9261acaa18ce9f69cc75218
James <jamesdwire66@gmail.com>
2024-04-02 19:20:52 -0600

    test

--- client/src/components/Locations.jsx
+++ client/src/components/Locations.jsx
@@ -1,29 +1,50 @@
-import React from 'react'; // Import React
+
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import locations from '../locations.js';
 function Locations(props) {
-   
+    const styles = {
+        card: {
+            width: '20rem',
+            margin: '1rem',
+            height: '18rem',
+            // display: 'flex',
+            // display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)" , 
+            // gridGap: 20,
+        },
+        image: {
+            height: '10rem',
+            objectFit: 'fill',
+            objectPosition: 'center',
+            display: 'fixed',
+        },
+        text: {
+            fontSize: '1rem',
+            textAlign: 'left',
+            display: 'flex',
+        }
+    }
+
 
     return (
         <>
-        {locations.map(location => (
-            <Card key={location.id} style={{ width: '20em',height: '18em' }}>
-                <Card.Img variant="top" src={location.image}  />
-                <Card.Body>
-                    <Card.Title>{location.name}</Card.Title>
-                    <Card.Text>
-                        Phone: {location.phone}
-                        <br />
-                       Address: {location.address}
-                       <br />
-                          Hours: {location.hours}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        ))}
-    </>
-);
+            {locations.map(location => (
+                <Card key={location.id} style={styles.card}>
+                    <Card.Img variant="top" src={location.image} style={styles.image} />
+                    <Card.Body style={styles.card}>
+                        <Card.Title>{location.name}</Card.Title>
+                        <Card.Text style={styles.text}>
+                            Phone: {location.phone}
+                            <br />
+                            Address: {location.address}
+                            <br />
+                            Hours: {location.hours}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
 }
 
 export default Locations;
