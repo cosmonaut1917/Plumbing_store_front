83a9c64dffd773e5149674949a0b1ce3e9bc1333
tronmitch <theron.mitchell@gmail.com>
2024-04-09 20:36:12 -0600

    changed store images to imports so they work on render

--- client/src/locations.js
+++ client/src/locations.js
@@ -1,9 +1,17 @@
-// This file contains the data for the locations page. Each location has a name, address, image, size, and phone number. The data is exported to the locations page to be displayed.
+// Importing images
+import richfieldImage from "../src/assets/images/richfield.jpg";
+import loganImage from "../src/assets/images/logan.jpg";
+import ogdenImage from "../src/assets/images/ogden.png";
+import saltLakeCityImage from "../src/assets/images/salt-lake-city.jpg";
+import oremImage from "../src/assets/images/orem.jpg";
+import cederCityImage from "../src/assets/images/ceder-city.jpg";
+
+// Locations data with image imports
 const locations = [
     {
         name: "Richfield",
         address: "90 North 600 East",
-        image: "../src/assets/images/richfield.jpg",
+        image: richfieldImage,
         phone: "435-896-8231",
         hours: "Monday-Friday 7:00am-5:30pm",
         map: { lat: 38.7723, lng: -112.0841 }
@@ -11,7 +19,7 @@ const locations = [
     {
         name: "Logan",
         address: "1060 West Center Street",
-        image: "../src/assets/images/logan.jpg",
+        image: loganImage,
         phone: "435-752-9580",
         hours: "Monday-Friday 7:00am-5:30pm",
         map: { lat: 41.73184, lng: -111.86136 }
@@ -19,7 +27,7 @@ const locations = [
     {
         name: "Ogden",
         address: "615 west 12th street",
-        image:"../src/assets/images/ogden.png",
+        image: ogdenImage,
         phone: " 385-333-0400",
         hours: "Monday-Friday 7:00am-5:00pm",
         map: { lat: 41.24443, lng: -111.99214 }
@@ -27,15 +35,15 @@ const locations = [
     {
         name: "Salt Lake City",
         address: "2069 South 700 West",
-        image:"../src/assets/images/salt-lake-city.jpg",
+        image: saltLakeCityImage,
         phone: "801-973-8700",
         hours: "Monday-Friday 7:00am-5:00pm",
         map: { lat: 40.72754, lng: -111.91116 }
     },
     {
-        name:"Orem",
+        name: "Orem",
         address: "1036 North 1430 West",
-        image:"../src/assets/images/orem.jpg",
+        image: oremImage,
         phone: " 801-434-9700",
         hours: "Monday-Friday 7:00am-5:00pm",
         map: { lat: 40.31626, lng: -111.72976 }
@@ -43,12 +51,11 @@ const locations = [
     {
         name: "Ceder City",
         address: "410 South Main Street",
-        image: "../src/assets/images/ceder-city.jpg",
+        image: cederCityImage,
         phone: "435-586-4440",
         hours: "Monday-Friday 7:00am-5:30pm",
         map: { lat: 37.67005, lng: -113.06165 }
-    },
-    
-]
+    },    
+];
 
 export default locations;
